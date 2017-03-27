abe1371b1ef6bd251ce6602cadd516d7bbde6f6b
Rodolfo Canessa <Rodolfo Canessa>
2017-03-27 15:58:34 -0600

    main components definition

--- client/app/pages/main/main.component.jsx
+++ client/app/pages/main/main.component.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import appHeader from '../../components/app-nav';
+import appNav from '../../components/app-nav';
 
-const mainPage = () => <div className="main-page">main page</div>;
+const mainPage = () => (
+	<div className="main-page">
+
+		<div className="main-page-row">
+			<div className="main-page-col-1">
+
+			</div>
+			<div className="main-page-col-2">
+				
+			</div>
+		</div>
+	</div>
+);
 
 export default mainPage;
